3190dece7f62e550e7f7a0879b6bf8cb9904af97
João Marcos <joao.santos160@fatec.sp.gov.br>
2021-12-02 19:09:01 -0300

    Estilização do footer e header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { faAngular, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Inteligência Artificial';
+  faAngleLeft = faAngleLeft;
+  faAngleRight = faAngleRight;
+  faAngular = faAngular;
+  faGithub = faGithub;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { TrainingComponent } from './pages/training/training.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { TrainingComponent } from './pages/training/training.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
